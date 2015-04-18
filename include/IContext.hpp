319b1c3b1cf8d3b1cfb841006dcf01b0d10c32d3
Jack Andersen <jackoalan@gmail.com>
2015-04-18 12:08:27 -1000

    restored IContext.hpp

--- include/IContext.hpp
+++ include/IContext.hpp
@@ -1,12 +1,22 @@
 #ifndef ICONTEXT_HPP
 #define ICONTEXT_HPP
 
+#include <string>
 
 class IContext
 {
 public:
-    IContext();
-    ~IContext();
+    virtual ~IContext() {}
+    
+    virtual void setMinVersion  (const int& min)=0;
+    virtual void setMajorVersion(const int& maj)=0;
+    virtual void create();
+    virtual const std::string version() const=0;
+    virtual const std::string name() const=0;
+    virtual int depthSize() const=0;
+    virtual int redDepth() const=0;
+    virtual int greenDepth() const=0;
+    virtual int blueDepth() const=0;
 };
 
 #endif // ICONTEXT_HPP
