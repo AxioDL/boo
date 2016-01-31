e792f8d581a392f6ad572ce6451e2a0f3b911dc2
Phillip Stephens <antidote.crk@gmail.com>
2016-01-31 00:37:23 -0800

    FreeBSD Stubs

---
+++ lib/inputdev/HIDDeviceBSD.cpp
@@ -0,0 +1,31 @@
+#include "boo/inputdev/IHIDListener.hpp"
+#include "boo/inputdev/DeviceFinder.hpp"
+
+
+namespace boo
+{
+
+class HIDListenerBSD final : public IHIDListener
+{
+    DeviceFinder& m_finder;
+public:
+    HIDListenerBSD(DeviceFinder& finder)
+        : m_finder(finder)
+    {
+    }
+
+    ~HIDListenerBSD()
+    {
+    }
+
+    bool startScanning() { return false; }
+    bool stopScanning() { return false; }
+
+    bool scanNow() { return false; }
+};
+
+IHIDListener* IHIDListenerNew(DeviceFinder &finder)
+{
+    return new HIDListenerBSD(finder);
+}
+}
