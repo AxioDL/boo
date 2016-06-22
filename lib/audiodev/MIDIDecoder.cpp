e4c625c55a82ed38bd9d53ba8ab7658de211306b
Jack Andersen <jackoalan@gmail.com>
2016-06-22 11:44:37 -1000

    MIDIDecoder bug fixes

--- lib/audiodev/MIDIDecoder.cpp
+++ lib/audiodev/MIDIDecoder.cpp
@@ -51,101 +51,23 @@ MIDIDecoder::receiveBytes(std::vector<uint8_t>::const_iterator begin,
     else
         it--;
 
-    uint8_t chan = m_status & 0xf;
-    switch (Status(m_status & 0xf0))
+    if (m_status == 0xff)
     {
-    case Status::NoteOff:
-    {
+        /* Meta events (ignored for now) */
         if (it == end)
             return begin;
         a = *it++;
-        if (it == end)
-            return begin;
-        b = *it++;
-        m_out.noteOff(chan, clamp7(a), clamp7(b));
-        break;
+
+        uint32_t length;
+        _readContinuedValue(it, end, length);
+        it += length;
     }
-    case Status::NoteOn:
+    else
     {
-        if (it == end)
-            return begin;
-        a = *it++;
-        if (it == end)
-            return begin;
-        b = *it++;
-        m_out.noteOn(chan, clamp7(a), clamp7(b));
-        break;
-    }
-    case Status::NotePressure:
-    {
-        if (it == end)
-            return begin;
-        a = *it++;
-        if (it == end)
-            return begin;
-        b = *it++;
-        m_out.notePressure(chan, clamp7(a), clamp7(b));
-        break;
-    }
-    case Status::ControlChange:
-    {
-        if (it == end)
-            return begin;
-        a = *it++;
-        if (it == end)
-            return begin;
-        b = *it++;
-        m_out.controlChange(chan, clamp7(a), clamp7(b));
-        break;
-    }
-    case Status::ProgramChange:
-    {
-        if (it == end)
-            return begin;
-        a = *it++;
-        m_out.programChange(chan, clamp7(a));
-        break;
-    }
-    case Status::ChannelPressure:
-    {
-        if (it == end)
-            return begin;
-        a = *it++;
-        m_out.channelPressure(chan, clamp7(a));
-        break;
-    }
-    case Status::PitchBend:
-    {
-        if (it == end)
-            return begin;
-        a = *it++;
-        if (it == end)
-            return begin;
-        b = *it++;
-        m_out.pitchBend(chan, clamp7(b) * 128 + clamp7(a));
-        break;
-    }
-    case Status::SysEx:
-    {
-        switch (Status(m_status & 0xff))
+        uint8_t chan = m_status & 0xf;
+        switch (Status(m_status & 0xf0))
         {
-        case Status::SysEx:
-        {
-            uint32_t len;
-            if (!_readContinuedValue(it, end, len) || end - it < len)
-                return begin;
-            m_out.sysex(&*it, len);
-            break;
-        }
-        case Status::TimecodeQuarterFrame:
-        {
-            if (it == end)
-                return begin;
-            a = *it++;
-            m_out.timeCodeQuarterFrame(a >> 4 & 0x7, a & 0xf);
-            break;
-        }
-        case Status::SongPositionPointer:
+        case Status::NoteOff:
         {
             if (it == end)
                 return begin;
@@ -153,40 +75,132 @@ MIDIDecoder::receiveBytes(std::vector<uint8_t>::const_iterator begin,
             if (it == end)
                 return begin;
             b = *it++;
-            m_out.songPositionPointer(clamp7(b) * 128 + clamp7(a));
+            m_out.noteOff(chan, clamp7(a), clamp7(b));
             break;
         }
-        case Status::SongSelect:
+        case Status::NoteOn:
         {
             if (it == end)
                 return begin;
             a = *it++;
-            m_out.songSelect(clamp7(a));
+            if (it == end)
+                return begin;
+            b = *it++;
+            m_out.noteOn(chan, clamp7(a), clamp7(b));
             break;
         }
-        case Status::TuneRequest:
-            m_out.tuneRequest();
+        case Status::NotePressure:
+        {
+            if (it == end)
+                return begin;
+            a = *it++;
+            if (it == end)
+                return begin;
+            b = *it++;
+            m_out.notePressure(chan, clamp7(a), clamp7(b));
             break;
-        case Status::Start:
-            m_out.startSeq();
+        }
+        case Status::ControlChange:
+        {
+            if (it == end)
+                return begin;
+            a = *it++;
+            if (it == end)
+                return begin;
+            b = *it++;
+            m_out.controlChange(chan, clamp7(a), clamp7(b));
             break;
-        case Status::Continue:
-            m_out.continueSeq();
+        }
+        case Status::ProgramChange:
+        {
+            if (it == end)
+                return begin;
+            a = *it++;
+            m_out.programChange(chan, clamp7(a));
             break;
-        case Status::Stop:
-            m_out.stopSeq();
+        }
+        case Status::ChannelPressure:
+        {
+            if (it == end)
+                return begin;
+            a = *it++;
+            m_out.channelPressure(chan, clamp7(a));
             break;
-        case Status::Reset:
-            m_out.reset();
+        }
+        case Status::PitchBend:
+        {
+            if (it == end)
+                return begin;
+            a = *it++;
+            if (it == end)
+                return begin;
+            b = *it++;
+            m_out.pitchBend(chan, clamp7(b) * 128 + clamp7(a));
             break;
-        case Status::SysExTerm:
-        case Status::TimingClock:
-        case Status::ActiveSensing:
+        }
+        case Status::SysEx:
+        {
+            switch (Status(m_status & 0xff))
+            {
+            case Status::SysEx:
+            {
+                uint32_t len;
+                if (!_readContinuedValue(it, end, len) || end - it < len)
+                    return begin;
+                m_out.sysex(&*it, len);
+                break;
+            }
+            case Status::TimecodeQuarterFrame:
+            {
+                if (it == end)
+                    return begin;
+                a = *it++;
+                m_out.timeCodeQuarterFrame(a >> 4 & 0x7, a & 0xf);
+                break;
+            }
+            case Status::SongPositionPointer:
+            {
+                if (it == end)
+                    return begin;
+                a = *it++;
+                if (it == end)
+                    return begin;
+                b = *it++;
+                m_out.songPositionPointer(clamp7(b) * 128 + clamp7(a));
+                break;
+            }
+            case Status::SongSelect:
+            {
+                if (it == end)
+                    return begin;
+                a = *it++;
+                m_out.songSelect(clamp7(a));
+                break;
+            }
+            case Status::TuneRequest:
+                m_out.tuneRequest();
+                break;
+            case Status::Start:
+                m_out.startSeq();
+                break;
+            case Status::Continue:
+                m_out.continueSeq();
+                break;
+            case Status::Stop:
+                m_out.stopSeq();
+                break;
+            case Status::Reset:
+                m_out.reset();
+                break;
+            case Status::SysExTerm:
+            case Status::TimingClock:
+            case Status::ActiveSensing:
+            default: break;
+            }
+            break;
+        }
         default: break;
         }
-        break;
-    }
-    default: break;
     }
 
     return it;
